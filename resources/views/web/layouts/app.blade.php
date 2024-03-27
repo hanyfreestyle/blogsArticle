da3732c482fb888fd9c56990f5e06f157671138f
Hany Darwish <hany.freestyle4u@gmail.com>
2024-03-27 21:37:07 +0200

    style

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -25,6 +25,7 @@
 {{--@include('web.layouts.inc.footer')--}}
 
 
+
 <div id="preloader"></div>
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
