041037f86a479e72cc1ab9404dabfe44bece3683
Hany Darwish <hany.freestyle4u@gmail.com>
2024-03-31 04:33:44 +0200

    WebPrivacy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,7 @@ class DatabaseSeeder extends Seeder {
 
     public function run(): void {
 
-        $this->call(PermissionSeeder::class);
+//        $this->call(PermissionSeeder::class);
         $this->call(AdminUserSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UsersTableSeeder::class);
