3828dbaf71215faa054a2d6255549a4d8d62d240
Hany Darwish <hany.freestyle4u@gmail.com>
2024-08-19 09:39:14 +0300

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,31 +11,17 @@ use App\AppCore\WebSettings\Seeder\SettingsTableSeeder;
 use App\AppCore\DefPhoto\DefPhotoSeeder;
 use App\AppCore\UploadFilter\Seeder\UploadFilterSeeder;
 use App\AppCore\Menu\AdminMenuSeeder;
-
-
-use App\AppPlugin\BlogPost\Models\Blog;
-use App\AppPlugin\BlogPost\Models\BlogCategory;
-use App\AppPlugin\BlogPost\Models\BlogCategoryTranslation;
-use App\AppPlugin\BlogPost\Models\BlogPivot;
-use App\AppPlugin\BlogPost\Models\BlogReview;
-use App\AppPlugin\BlogPost\Models\BlogTranslation;
 use App\AppPlugin\BlogPost\Seeder\BlogPostSeeder;
-
-
 use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder;
 use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder2;
 use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder3;
 use App\AppPlugin\BlogPost\Seeder\BlogTranslationSeeder4;
-use App\AppPlugin\BlogPost\Seeder\PivotSeeder;
 use App\AppPlugin\Config\Meta\SeederMetaTag;
 use App\AppPlugin\Config\Privacy\SeederWebPrivacy;
 use App\AppPlugin\BlogPost\Seeder\BlogCategorySeeder;
-
-
 use App\AppPlugin\Config\SiteMap\GoogleCodeSeeder;
 use App\AppPlugin\Pages\Seeder\PageSeeder;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder {
@@ -55,10 +41,6 @@ class DatabaseSeeder extends Seeder {
 
             if (File::isFile(base_path('routes/AppPlugin/config/siteMaps.php'))) {
                 $this->call(GoogleCodeSeeder::class);
-            }
-
-            if (File::isFile(base_path('routes/AppPlugin/crm/ImportData.php'))) {
-                $this->call(ImportDataSeeder::class);
             }
 
             if (File::isFile(base_path('routes/AppPlugin/config/configMeta.php'))) {
